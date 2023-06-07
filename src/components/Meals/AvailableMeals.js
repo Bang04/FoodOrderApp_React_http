dd67cbff961db1ef0234def4909a58b816a78eaa
younme20 <eunkyeong_b@naver.com>
2023-06-07 23:30:22 +0900

    feat :  에러 예외 처리 소스 추가

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,11 +8,18 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
 
 
   useEffect(async() => {
     const fetchMeals = async () => {
-        const response =  await fetch('https://react-http-fe464-default-rtdb.firebaseio.com/meals.json').then();
+        const response =  await fetch('https://react-http-fe464-default-rtdb.firebaseio.com/meals.json');
+
+        if(!response.ok){
+          throw new Error('Something went wrong!');
+        }
+
         const responseData = await response.json();
 
         const loadedMeals = [];
@@ -29,14 +36,22 @@ const AvailableMeals = () => {
         setMeals(loadedMeals);
         setIsLoading(false);
     }
-    fetchMeals();
+
+  
+      fetchMeals().catch(error => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
   }, []);
 
   if(isLoading){
     return <section className={classes.MealsLoading}>
-
         <p>Loading....</p>
     </section>
+  }
+
+  if(httpError){
+    return <section className={classes.MealsError}><p>{httpError}</p></section>
   }
   const mealsList = meals.map((meal) => (
     <MealItem
